c30114cd5a2a137b88ad45cf40c1f6b2f24aaa63
kaapomoi <kaapomo@gmail.com>
2023-01-20 20:59:24 +0200

    Joku ajaa teslaa kun avaruusalusta...

--- esimerkit/terve_maailma.cpp
+++ esimerkit/terve_maailma.cpp
@@ -7,7 +7,7 @@ nimiavaruus
 {
     sapluuna<tyyppinimi T> mitätön tulosta(T vakio & tulostettava) epäpoikkeuksellinen
     {
-        normi::ulos << merkkijonoksi(tulostettava);
+        ulos << normi::merkkijonoksi(tulostettava) << "\n";
     }
 
     sapluuna<> mitätön tulosta<merkkijono>(normi::merkkijono vakio & tulostettava)
@@ -17,29 +17,62 @@ nimiavaruus
     }
 }
 
-luku pää()
+luokka Avaruusalus
 {
-    luku a{1};
-    luku b{2};
-    luku c = a + b;
+julkinen:
+    Avaruusalus(merkkijono vakio & nimi) epäpoikkeuksellinen : m_nimi(nimi) {}
 
-    kun(c >= 0)
+    mitätön aja(liuku vakio matka) epäpoikkeuksellinen
     {
-        mikä(c)
-        {
-            keissi 1:
-            {
-                merkkijono vakio terve_maailma{"Terve, Maailma!\n"};
-                tulosta(terve_maailma);
-                riko;
-            }
-        oletus : {
-            tulosta(merkkijono("Vautsivau\n"));
-            riko;
-        }
-        }
-        c--;
+        ulos << m_nimi << "::Sit mentiin\n";
+        m_kuljettu_matka += matka;
     }
 
-    palauta c;
+    luku ajettu_matka() epäpoikkeuksellinen { palauta m_kuljettu_matka / 1; }
+
+yksityinen:
+    merkkijono const m_nimi;
+    liuku m_kuljettu_matka{0.0f};
+};
+
+staattinen vakioekspr tupla nopeus{1.56};
+
+luku pää()
+{
+    luku valmisteluaika{2};
+    luku aikataulun_säätövara{1};
+    luku lähtölaskenta{valmisteluaika + aikataulun_säätövara};
+
+    tupla vakio aika{3};
+
+    Avaruusalus alus{"Rakettispagetti"};
+
+    kun(lähtölaskenta >= 0)
+    {
+        jos(lähtölaskenta == 2) { ulos << "Joo...kohta mennään\n"; }
+        muuten
+        {
+            mikä(lähtölaskenta)
+            {
+                keissi 0:
+                {
+                    merkkijono vakio terve_maailma{"Tervemenoa, maailma!"};
+                    tulosta(terve_maailma);
+                    riko;
+                }
+                keissi 1:
+                {
+                    alus.aja(nopeus * aika);
+                    riko;
+                }
+            oletus : {
+                tulosta(merkkijono("Vautsivau"));
+                riko;
+            }
+            }
+        }
+        lähtölaskenta--;
+    }
+
+    palauta alus.ajettu_matka();
 }
